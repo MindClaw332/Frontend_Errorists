7c766ec3634ce47f31a42c43454796efddf1f100
carinezoe <zoecleo.dreessen@gmail.com>
2025-01-13 20:34:14 +0100

    display data/generate buttons

--- src/app/points/points.component.ts
+++ src/app/points/points.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-points',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './points.component.html',
   styleUrl: './points.component.css'
 })
 export class PointsComponent {
 
+// Visibility
 isHidden = true;
 isVisible = false;
 
@@ -19,6 +21,42 @@ viewPoints () {
 viewTests () {
   this.isHidden = !this.isHidden;
   this.isVisible = ! this.isVisible;
+}
+
+//arrays
+tests = [
+  { id: 1, name: 'wiskunde'},
+  { id: 2, name: 'biologie'},
+  { id: 3, name: 'natuurkunde'},
+  { id: 4, name: 'chemie'},
+  { id: 5, name: 'aarderijkskunde'}
+];
+
+students = [
+  { id: 1, firstname: 'Jeff', lastname: 'Dunn', test_id: 1, value: 16, maxvalue: 20},
+  { id: 2, firstname: 'Lisa', lastname: 'Pruet', test_id: 1, value: 13, maxvalue: 20},
+  { id: 3, firstname: 'Leo', lastname: 'Durett', test_id: 2, value: 8, maxvalue: 20},
+  { id: 4, firstname: 'Liam', lastname: 'Bay', test_id: 1, value: 12, maxvalue: 20},
+  { id: 5, firstname: 'Jamie', lastname: 'Baker', test_id: 2, value: 15, maxvalue: 20},
+  { id: 6, firstname: 'Jeff', lastname: 'Olan', test_id: 3, value: 17, maxvalue: 20},
+  { id: 7, firstname: 'Brady', lastname: 'Duran', test_id: 3, value: 5, maxvalue: 20},
+  { id: 8, firstname: 'Remy', lastname: 'Duran', test_id: 4, value: 13, maxvalue: 20},
+  { id: 9, firstname: 'Liz', lastname: 'Mayfair', test_id: 2, value: 14, maxvalue: 20},
+  { id: 10, firstname: 'Noelle', lastname: 'Reaper', test_id: 4, value: 12, maxvalue: 20}
+];
+
+//filter the student array for needed students
+filteredStudents: Array<{ id: number; firstname: string; lastname: string; test_id: number; value: number; maxvalue: number }> = []
+
+displayStudents(test: number): void {
+  this.filteredStudents = this.students.filter(student => student.test_id === test);
+}
+
+//filter the test array for needed test
+selectedTest: { id: number; name: string } | null = null;
+
+selectTest(test: { id: number; name: string }): void {
+  this.selectedTest = test;
 }
 
 }
--- src/app/teacher/teacher.component.ts
+++ src/app/teacher/teacher.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 })
 export class TeacherComponent {
 
+//visibility
 isHidden = true;
 isVisible = false;
   
@@ -19,6 +20,44 @@ viewStudents () {
 viewClasses () {
   this.isHidden = !this.isHidden;
   this.isVisible = ! this.isVisible;
+}
+
+//arrays
+classes = [
+  { id: 1, name: '3wis'},
+  { id: 2, name: '2wis'},
+  { id: 3, name: '3eco'},
+  { id: 4, name: '2eco'},
+  { id: 5, name: '4eco'},
+  { id: 6, name: '4wis'}
+];
+
+students = [
+  { id: 1, firstname: 'Jeff', lastname: 'Dunn', class_id: 1},
+  { id: 2, firstname: 'Lisa', lastname: 'Pruet', class_id: 1},
+  { id: 3, firstname: 'Leo', lastname: 'Durett', class_id: 2},
+  { id: 4, firstname: 'Liam', lastname: 'Bay', class_id: 1},
+  { id: 5, firstname: 'Jamie', lastname: 'Baker', class_id: 2},
+  { id: 6, firstname: 'Jeff', lastname: 'Olan', class_id: 3},
+  { id: 7, firstname: 'Brady', lastname: 'Duran', class_id: 3},
+  { id: 8, firstname: 'Remy', lastname: 'Duran', class_id: 4},
+  { id: 9, firstname: 'Liz', lastname: 'Mayfair', class_id: 2},
+  { id: 10, firstname: 'Noelle', lastname: 'Reaper', class_id: 4}
+];
+
+
+//filter student array for needed students
+filteredStudents: Array<{ id: number; firstname: string; lastname: string; class_id: number }> = []
+
+displayStudents(classId: number): void {
+  this.filteredStudents = this.students.filter(student => student.class_id === classId);
+}
+
+//filter class array for needed class
+selectedClass: { id: number; name: string } | null = null;
+
+selectClass(classItem: { id: number; name: string }): void {
+  this.selectedClass = classItem;
 }
 
 }
