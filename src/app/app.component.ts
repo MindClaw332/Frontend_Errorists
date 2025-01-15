9ac8cea79d9f55d9a78fa44ef064d8cb666458b6
Michiel Konings <michiel.konings@icloud.com>
2025-01-15 15:52:49 +0100

    implemented css for login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { StudentsComponent } from "./students/students.component";
-import {TeacherComponent} from "./teacher/teacher.component"
+import { LoginComponent } from "./login/login.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, StudentsComponent, TeacherComponent],
+  imports: [LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
