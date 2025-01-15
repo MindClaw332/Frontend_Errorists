56340dc6979527ebf525f49f9fa3fa492eb6e281
Michiel Konings <michiel.konings@icloud.com>
2025-01-15 15:06:28 +0100

    finished group css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { StudentsComponent } from "./students/students.component";
-import {TeacherComponent} from "./teacher/teacher.component"
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, StudentsComponent, TeacherComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
