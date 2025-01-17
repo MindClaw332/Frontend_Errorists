9fe3702121d13ce51a008082763f9706aab96104
Michiel Konings <michiel.konings@icloud.com>
2025-01-17 11:24:20 +0100

    final changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { LoginComponent } from "./login/login.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
