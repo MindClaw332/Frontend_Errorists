dac5b1e4f1befef67930d995f7f92be3cfb0e342
carinezoe <zoecleo.dreessen@gmail.com>
2025-01-17 11:12:23 +0100

    Added support for new API calls in interfaces

--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -2,12 +2,24 @@ export interface User {
     id:number,
     firstname: string,
     lastname: string,
-    email: string,
-    password: string,
     eligible: number,
     role: string,
     class_id:number,
     class: string,
     schoolyear: number,
-    average: number
+    average: number,
+    tests: [
+        {
+            test_name: string,
+            test_value: number,
+            test_maxvalue: number
+        }
+    ],
+    groups: [
+        {
+            id: number,
+            group_name: string,
+            course_name: string
+        }
+    ]
 }
