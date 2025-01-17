42fd50615abc36b3bbcd86f26263b713e35c9c9a
Michiel Konings <michiel.konings@icloud.com>
2025-01-17 16:14:02 +0100

    added html and part of the css for batchadd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { BatchaddComponent } from "./batchadd/batchadd.component";
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [BatchaddComponent]
 })
 export class AppComponent {
   title = 'frontend_erorrists';
