970fa5e5ab41e1c799263a4ba61d5aeec39d4b1c
Michiel Konings <michiel.konings@icloud.com>
2025-01-20 15:12:53 +0100

    started on batch add funconalitie

--- src/app/batchadd/batchadd.component.ts
+++ src/app/batchadd/batchadd.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, numberAttribute } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { last } from 'rxjs';
 
 @Component({
   selector: 'app-batchadd',
@@ -7,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './batchadd.component.css'
 })
 export class BatchaddComponent {
+
+addUser(firstName: string,lastName: string,password: string, email: string,role: number)
+{
+let userInfo: string[] = [firstName, lastName,password, email, role.toString()]
+console.log(userInfo)
+}
 
 }
