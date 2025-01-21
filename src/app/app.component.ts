68f7d0092cccd69130f0e1db6079ab7e9e29362f
Michiel Konings <michiel.konings@icloud.com>
2025-01-21 12:52:59 +0100

    some small changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { BatchaddComponent } from "./batchadd/batchadd.component";
+
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  imports: [BatchaddComponent]
 })
 export class AppComponent {
   title = 'frontend_erorrists';
