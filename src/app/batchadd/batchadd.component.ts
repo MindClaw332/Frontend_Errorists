5d5c1e00cca2332c045177a59dcc7bd8f69460c8
Michiel Konings <michiel.konings@icloud.com>
2025-01-21 14:34:16 +0100

    started work on implemanting the api

--- src/app/batchadd/batchadd.component.ts
+++ src/app/batchadd/batchadd.component.ts
@@ -1,6 +1,9 @@
-import { Component, numberAttribute } from '@angular/core';
+import { Component, inject, numberAttribute } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { last } from 'rxjs';
+import { LoginService } from '../login.service';
+import { ClassdataService } from '../shared/classdata.service';
+import { first } from 'rxjs';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-batchadd',
@@ -10,10 +13,49 @@ import { last } from 'rxjs';
 })
 export class BatchaddComponent {
 
-addUser(firstName: string,lastName: string,password: string, klas:string, email: string,role: number)
+private registrationdata = inject(LoginService);
+private classdata = inject(ClassdataService);
+
+//signals
+registration = this.registrationdata.registration;
+classes = this.classdata.classes;
+
+//constructor
+constructor(private router :Router)
 {
-let userInfo: string[] = [firstName, lastName,password,klas, email, role.toString()]
-console.log(userInfo)
+  this.classdata.loadClasses();
+  
 }
 
+//visibility
+isHidden = true;
+isVisible = false;
+
+//for vieuwing classes
+viewClasses()
+{
+  this.isHidden = !this.isHidden;
+  this.isVisible = !this.isVisible;
+}
+
+//fillter class array for needed classes
+selectedClass: {id: number; name:string} | null = null;
+
+selectClass(classItem:{id:number; name:string}): void{
+  this.selectedClass = classItem;
+}
+
+//function to add users to the data base 
+addUser(firstName: string,lastName: string,password: string, klas:number, email: string,role: number)
+{
+  const user = {
+    firstName,
+    lastName,
+    email, 
+    password,
+    klas,
+    role,
+  };
+  
+}
 }
